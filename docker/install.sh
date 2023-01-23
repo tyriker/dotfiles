0211e99079738ab0f8eaf159c887e82071f95463
Tyler Riker <tyler@wewritecode.com>
2023-01-22 18:43:01 -0600

    install docker if not installed

--- docker/install.sh
+++ docker/install.sh
@@ -1,3 +1,16 @@
-echo 'Installing Docker Desktop'
-brew install --cask docker
+#!/bin/sh
+#
+# Docker
+#
+# This installs Docker
 
+# Check for Docker
+if test ! $(which Docker)
+then
+  echo "  Installing Docker for you."
+
+  brew install --cask docker
+
+fi
+
+exit 0
