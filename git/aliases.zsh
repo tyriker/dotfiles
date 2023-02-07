2d0b0fd5ae402dc76254eaf171430eac80258294
Tyler Riker <tyler@wewritecode.com>
2023-02-07 13:40:13 -0600

    set push.default to upstream

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -27,5 +27,3 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
 alias gm='git merge'
-alias gpsup='git push --set-upstream origin $(git_current_branch)'
-
