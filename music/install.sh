1c90b3aefe0d5f981011076acc9b8bf35632be6c
Tyler Riker <tyler@wewritecode.com>
2023-06-28 15:47:35 -0500

    add noTunes to startup

--- music/install.sh
+++ music/install.sh
@@ -1,2 +1,7 @@
 brew install --cask notunes
 
+osascript -e 'tell application "System Events" to delete login item 
+"noTunes"'
+osascript -e 'tell application "System Events" to make login item at end 
+with properties {name: "noTunes", path:"/Applications/noTunes.app", 
+hidden:false}'
