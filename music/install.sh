2a5e0d9cfea0076ed2946477beb63240a1a536c8
Tyler Riker <tyler@wewritecode.com>
2023-11-28 10:51:28 -0600

    fix noTunes install

--- music/install.sh
+++ music/install.sh
@@ -1,7 +1,4 @@
 brew install --cask notunes
 
-osascript -e 'tell application "System Events" to delete login item 
-"noTunes"'
-osascript -e 'tell application "System Events" to make login item at end 
-with properties {name: "noTunes", path:"/Applications/noTunes.app", 
-hidden:false}'
+osascript -e 'tell application "System Events" to delete login item "noTunes"'
+osascript -e 'tell application "System Events" to make login item at end with properties {name: "noTunes", path:"/Applications/noTunes.app", hidden:false}'
