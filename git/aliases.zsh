0c9b0f1db711c3e760de304f1797c9f56ebb790d
Tyler Riker <tyler@wewritecode.com>
2023-12-05 13:25:24 -0600

    remove redundancy in git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,26 +1,7 @@
+# See .gitconfig.symlink for list of git aliases
 for al in `git --list-cmds=alias`; do
     alias g$al="git $al"
 done
 
-# The rest of my fun git aliases
-#alias gl='git pull --prune'
-#alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-#alias gp='git push origin HEAD'
-
-# Remove `+` and `-` from start of diff lines; just rely upon color.
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
-
-#alias gc='git commit'
-#alias gca='git commit -a'
-#alias gco='git checkout'
-#alias gcb='git checkout -b'
 #alias gcb='git copy-branch-name'
-#alias gcd='git checkout develop'
-#alias gb='git branch'
-#alias gf='git fetch'
-#alias gfa='git fetch --all'
-#alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-#alias gst='gs'
-#alias gac='git add -A && git commit -m'
 #alias ge='git-edit-new'
-#alias gm='git merge'
