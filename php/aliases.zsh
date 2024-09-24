722842ebf5df340d70c1d7be7d72518809b68673
Tyler Riker <tyler@wewritecode.com>
2024-09-24 15:25:06 -0500

    peg php and composer versions

--- php/aliases.zsh
+++ php/aliases.zsh
@@ -1,3 +1,3 @@
-alias php='docker run --rm -ti -v php-composer-cache:/root/.composer -v $(pwd):/usr/src/app --workdir="/usr/src/app" php:latest'
-alias composer='docker run --rm -ti -v php-composer-cache:/tmp -v $(pwd):/usr/src/app --workdir="/usr/src/app" composer:latest'
+alias php='docker run --rm -ti -v php-composer-cache:/root/.composer -v $(pwd):/usr/src/app --workdir="/usr/src/app" php:8.1.0'
+alias composer='docker run --rm -ti -v php-composer-cache:/tmp -v $(pwd):/usr/src/app --workdir="/usr/src/app" composer:2.6.6'
 
