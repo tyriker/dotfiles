422d7d419b94db43f556eed9684371c4e9bf705c
Tyler Riker <tyler@wewritecode.com>
2024-11-07 16:42:20 -0600

    quote pwd command

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -1,1 +1,1 @@
-alias aws='docker run --rm -ti -v ~/.aws:/root/.aws -v $(pwd):/aws public.ecr.aws/aws-cli/aws-cli'
+alias aws='docker run --rm -ti -v ~/.aws:/root/.aws -v "$(pwd)":/aws public.ecr.aws/aws-cli/aws-cli'
