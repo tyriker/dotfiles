9b4213392929ebc0dfeccbf18608f6ea9323687f
Tyler Riker <tyler@wewritecode.com>
2025-03-15 20:49:15 -0400

    add some security tools

---
+++ security/aliases.zsh
@@ -0,0 +1,4 @@
+alias nmap='docker run --rm -it instrumentisto/nmap'
+alias ncat='docker run --rm -it --entrypoint ncat instrumentisto/nmap'
+alias nping='docker run --rm -it --entrypoint nping instrumentisto/nmap'
+
