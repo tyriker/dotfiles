20cf8b54a078301c12634f46e1afb863684777eb
Tyler Riker <tyler@wewritecode.com>
2025-03-15 20:48:35 -0400

    add various linux boxes

---
+++ linux/aliases.zsh
@@ -0,0 +1,4 @@
+alias alpine='docker run -it -v $(pwd):/data alpine'
+alias busybox='docker run -it --rm -v $(pwd):/data busybox'
+alias debian='docker run -it -v $(pwd):/data debian'
+alias ubuntu='docker run -it -v $(pwd):/data ubuntu'
