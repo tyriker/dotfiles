11b4015ecbf5e78bb7ffd335e09fb81444f01bd1
Tyler Riker <tyler@wewritecode.com>
2025-10-06 13:46:20 -0500

    replace tilde with /Users/tyriker

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -1,2 +1,3 @@
+
 alias aws='docker run --rm -ti -v ~/.aws:/root/.aws -v "$(pwd)":/aws public.ecr.aws/aws-cli/aws-cli'
-alias cdk='docker build -t my-aws-cdk:latest -f "~/.dotfiles/aws/cdk/Dockerfile" "~/.dotfiles/aws/cdk" && docker run --rm my-aws-cdk:latest -ti -v ~/.aws:/root/.aws -v "$(pwd)":/aws '
+alias cdk='docker build -t my-aws-cdk:latest -f "$HOME/.dotfiles/aws/cdk/Dockerfile" "$HOME/.dotfiles/aws/cdk" && docker run --rm my-aws-cdk:latest -ti -v ~/.aws:/root/.aws -v "$(pwd)":/aws '
