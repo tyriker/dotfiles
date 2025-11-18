00aeb6a95cd507ac83657fb91d4575fd77955e8c
Tyler Riker <tyler@wewritecode.com>
2025-11-18 13:07:54 -0600

    fix cdk

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -1,2 +1,2 @@
 alias aws='docker run --rm -ti -v ~/.aws:/root/.aws -v "$(pwd)":/aws public.ecr.aws/aws-cli/aws-cli'
-alias cdk='docker build -t my-aws-cdk:latest -f ~/.dotfiles/aws/cdk/Dockerfile ~/.dotfiles/aws/cdk && docker run --rm -ti -v ~/.aws:/root/.aws -v "$(pwd)":/aws my-aws-cdk:latest cdk'
+alias cdk='docker build -t my-aws-cdk:latest -f ~/.dotfiles/aws/cdk/Dockerfile ~/.dotfiles/aws/cdk && docker run --rm -ti -v ~/.aws:/root/.aws -v "$(pwd)":/aws my-aws-cdk:latest'
