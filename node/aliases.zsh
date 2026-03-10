6099ecd6e949d33fe32cdccab5fea4f2a6b45398
Tyler Riker <tyler@wewritecode.com>
2026-03-10 02:54:54 -0500

    native node instead of alias

--- node/aliases.zsh
+++
@@ -1,3 +0,0 @@
-alias node='docker run --rm -ti -v ~/.npm:/home/node/.npm -v $(pwd):/home/node/app --workdir="/home/node/app" node:latest node'
-alias npm='docker run --rm -ti -v ~/.npm:/home/node/.npm -v $(pwd):/home/node/app --workdir="/home/node/app" node:latest npm'
-
