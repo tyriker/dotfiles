99174cc05d7b067a04f4e692df402769dec8ae36
Tyler Riker <tyler@wewritecode.com>
2026-08-12 16:58:05 +0000

    uninstall legacy remote client first

--- remote/install.sh
+++ remote/install.sh
@@ -1,6 +1,7 @@
 brew install --cask tigervnc-viewer
-brew install --cask windows-app
 
 if brew list --cask microsoft-remote-desktop >/dev/null 2>&1; then
   brew uninstall --cask microsoft-remote-desktop
 fi
+
+brew install --cask windows-app
